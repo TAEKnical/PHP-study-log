d4527b77765d165ab7d28b399944148220a0a433
Jintae Lee <37683734+TAEKnical@users.noreply.github.com>
2018-08-01 17:36:23 +0900

    Day2
    data.zip -> data (directory)

---
+++ comparison.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+      <h1>Copmarison Operator &amp; Boolean data type</h1>
+      <h1>print(1);</h1>
+      <?php
+        print(1);
+        ?>
+        <h1>var_dump(1);</h1>
+        <?php
+        var_dump(1);
+         ?>
+         <h1>var_dump(1+1);</h1>
+         <?php
+         var_dump(1+1);
+          ?>
+        <h1>var_dump(11);</h1>
+        <?php
+        var_dump(11);
+         ?>
+         <h1>var_dump('11');</h1>
+        <?php
+        var_dump('11');
+         ?>
+         <h1>var_dump(1==1);</h1>
+        <?php
+        var_dump(1==1);
+         ?>
+  </body>
+</html>
