680d01601a7ca75a2c5ce60aad3fcd954e0b726e
Jintae Lee <37683734+TAEKnical@users.noreply.github.com>
2018-08-02 23:05:45 +0900

    Day3

--- index.php
+++ index.php
@@ -6,22 +6,28 @@
   <body>
     <h1><a href = "index.php">WEB</a></h1>
     <ol>
-      <li><a href="index.php?id=HTML">HTML</a></li>
-      <li><a href="index.php?id=CSS">CSS</a></li>
-      <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+      <?php
+      $list = scandir("./data");
+      var_dump($list)
+       ?>
     </ol>
     <h2>
       <?php
-      if(isset()){
+      if(isset($_GET["id"])){
         echo $_GET["id"];
       }
-    else{
-      echo "Welcome";
-    }
-       ?>
+      else{
+        echo "Welcome";
+      }
+      ?>
     </h2>
     <?php
+    if(isset($_GET["id"])){
       echo file_get_contents("data/".$_GET["id"]);
+    }
+    else{
+      echo "hello php";
+    }
     ?>
   </body>
 </html>
