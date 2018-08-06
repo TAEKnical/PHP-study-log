a8e187aa98dac6fffec2d4e98328e6d8e62f62e0
Jintae Lee <37683734+TAEKnical@users.noreply.github.com>
2018-08-06 22:22:55 +0900

    Day4

---
+++ function2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>function</title>
+  </head>
+  <body>
+    <h1>function</h1>
+    <h2>Basic</h2>
+    <?php
+      function basic(){
+        print("basic함수사용");
+      }
+      basic();
+     ?>
+  </body>
+</html>
