a3918d161e9c3731302fcdb06646616e6da58b5a
Jintae Lee <37683734+TAEKnical@users.noreply.github.com>
2018-08-07 17:57:59 +0900

    Day5(update)

--- update.php
+++ update.php
@@ -21,11 +21,12 @@
   }
 
   function print_description(){
-    if(isset($_GET['id'])){
-      echo file_get_contents("data/".$_GET['id']);
-    } else {
-      echo "Hello, PHP";
-    }
+      if(isset($_GET['id'])){
+        echo file_get_contents("data/".$_GET['id']);
+      }
+      else {
+        echo "hello php";
+      }
   }
 ?>
 
@@ -43,16 +44,18 @@
        ?>
     </ol>
     <a href = "create.php">create</a>
-    <?php if(isset($_GET['id'])) { ?>
-      <a href="update.php?id=<?=$_GET['id']?>">update</a>
+    <?php if(isset($_get['id'])) { ?>
+      <a href="update.php?id=<?php echo $_GET['id'];?>">update</a>
     <?php } ?>
+
     <h2>
-    <?php
-      print_title();
-    ?>
-  </h2>
-    <?php
-    print_description();
-     ?>
+      <form action = "update_process.php" method="post">
+        <input type="hidden" name="old_title" value="<?php echo $_GET['id'] ?>"
+        <p><input type="text" name="title" value="<?php print_title(); ?>"</p>
+        <p><textarea name="description" value="<?php print_description(); ?>"></textarea></p>
+        <p><input type="submit"></p>
+      </form>
+    </h2>
+
   </body>
 </html>
